282d3dfd80fc285efb4d1f883433067641c08418
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-05-13 20:08:58 -0400

    build: requirements.txt added

---
+++ install.sh
@@ -0,0 +1,3 @@
+python3 -m pip install numpy
+python3 -m pip install matplotlib
+pip3 install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
