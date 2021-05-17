24a92382ebf8f636bba645f729a8268f9cc17d1b
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-05-17 00:27:54 -0400

    style: code format changed

--- EDSR.py
+++ EDSR.py
@@ -3,27 +3,33 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.model_zoo 
+import torch.utils.model_zoo
 import torch.nn as nn
 
 url = {
-    'r16f64x2': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_baseline_x2-1bc95232.pt',
-    'r16f64x3': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_baseline_x3-abf2a44e.pt',
-    'r16f64x4': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_baseline_x4-6b446fab.pt',
-    'r32f256x2': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_x2-0edfb8a3.pt',
-    'r32f256x3': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_x3-ea3ef2c6.pt',
-    'r32f256x4': 'https://cv.snu.ac.kr/research/EDSR/models/edsr_x4-4f62e9ef.pt'
+    "r16f64x2": "https://cv.snu.ac.kr/research/EDSR/models/edsr_baseline_x2-1bc95232.pt",
+    "r16f64x3": "https://cv.snu.ac.kr/research/EDSR/models/edsr_baseline_x3-abf2a44e.pt",
+    "r16f64x4": "https://cv.snu.ac.kr/research/EDSR/models/edsr_baseline_x4-6b446fab.pt",
+    "r32f256x2": "https://cv.snu.ac.kr/research/EDSR/models/edsr_x2-0edfb8a3.pt",
+    "r32f256x3": "https://cv.snu.ac.kr/research/EDSR/models/edsr_x3-ea3ef2c6.pt",
+    "r32f256x4": "https://cv.snu.ac.kr/research/EDSR/models/edsr_x4-4f62e9ef.pt",
 }
+
 
 def default_conv(in_channels, out_channels, kernel_size, bias=True):
     return nn.Conv2d(
-        in_channels, out_channels, kernel_size,
-        padding=(kernel_size//2), bias=bias)
+        in_channels, out_channels, kernel_size, padding=(kernel_size // 2), bias=bias
+    )
+
 
 class MeanShift(nn.Conv2d):
     def __init__(
-        self, rgb_range,
-        rgb_mean=(0.4488, 0.4371, 0.4040), rgb_std=(1.0, 1.0, 1.0), sign=-1):
+        self,
+        rgb_range,
+        rgb_mean=(0.4488, 0.4371, 0.4040),
+        rgb_std=(1.0, 1.0, 1.0),
+        sign=-1,
+    ):
 
         super(MeanShift, self).__init__(3, 3, kernel_size=1)
         std = torch.Tensor(rgb_std)
@@ -32,10 +38,19 @@ class MeanShift(nn.Conv2d):
         for p in self.parameters():
             p.requires_grad = False
 
+
 class BasicBlock(nn.Sequential):
     def __init__(
-        self, conv, in_channels, out_channels, kernel_size, stride=1, bias=False,
-        bn=True, act=nn.ReLU(True)):
+        self,
+        conv,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        bias=False,
+        bn=True,
+        act=nn.ReLU(True),
+    ):
 
         m = [conv(in_channels, out_channels, kernel_size, bias=bias)]
         if bn:
@@ -45,10 +60,18 @@ class BasicBlock(nn.Sequential):
 
         super(BasicBlock, self).__init__(*m)
 
+
 class ResBlock(nn.Module):
     def __init__(
-        self, conv, n_feats, kernel_size,
-        bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
+        self,
+        conv,
+        n_feats,
+        kernel_size,
+        bias=True,
+        bn=False,
+        act=nn.ReLU(True),
+        res_scale=1,
+    ):
 
         super(ResBlock, self).__init__()
         m = []
@@ -68,19 +91,20 @@ class ResBlock(nn.Module):
 
         return res
 
+
 class Upsampler(nn.Sequential):
     def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
 
         m = []
-        if (scale & (scale - 1)) == 0:    # Is scale = 2^n?
+        if (scale & (scale - 1)) == 0:  # Is scale = 2^n?
             for _ in range(int(math.log(scale, 2))):
                 m.append(conv(n_feats, 4 * n_feats, 3, bias))
                 m.append(nn.PixelShuffle(2))
                 if bn:
                     m.append(nn.BatchNorm2d(n_feats))
-                if act == 'relu':
+                if act == "relu":
                     m.append(nn.ReLU(True))
-                elif act == 'prelu':
+                elif act == "prelu":
                     m.append(nn.PReLU(n_feats))
 
         elif scale == 3:
@@ -88,51 +112,52 @@ class Upsampler(nn.Sequential):
             m.append(nn.PixelShuffle(3))
             if bn:
                 m.append(nn.BatchNorm2d(n_feats))
-            if act == 'relu':
+            if act == "relu":
                 m.append(nn.ReLU(True))
-            elif act == 'prelu':
+            elif act == "prelu":
                 m.append(nn.PReLU(n_feats))
         else:
             raise NotImplementedError
 
         super(Upsampler, self).__init__(*m)
-        
+
+
 def make_model(args, parent=False):
-  print(args)
-  return EDSR(args)
+    print(args)
+    return EDSR(args)
+
 
 class EDSR(nn.Module):
     def __init__(self, args, conv=default_conv):
         super(EDSR, self).__init__()
 
-        n_resblocks = args['n_resblocks']
-        n_feats = args['n_feats']
-        kernel_size = 3 
-        scale = args['scale'][0]
+        n_resblocks = args["n_resblocks"]
+        n_feats = args["n_feats"]
+        kernel_size = 3
+        scale = args["scale"][0]
         act = nn.ReLU(True)
-        url_name = 'r{}f{}x{}'.format(n_resblocks, n_feats, scale)
+        url_name = "r{}f{}x{}".format(n_resblocks, n_feats, scale)
         if url_name in url:
             self.url = url[url_name]
         else:
             self.url = None
-        self.sub_mean = MeanShift(args['rgb_range'])
-        self.add_mean = MeanShift(args['rgb_range'], sign=1)
+        self.sub_mean = MeanShift(args["rgb_range"])
+        self.add_mean = MeanShift(args["rgb_range"], sign=1)
 
         # define head module
-        m_head = [conv(args['n_colors'], n_feats, kernel_size)]
+        m_head = [conv(args["n_colors"], n_feats, kernel_size)]
 
         # define body module
         m_body = [
-            ResBlock(
-                conv, n_feats, kernel_size, act=act, res_scale=args['res_scale']
-            ) for _ in range(n_resblocks)
+            ResBlock(conv, n_feats, kernel_size, act=act, res_scale=args["res_scale"])
+            for _ in range(n_resblocks)
         ]
         m_body.append(conv(n_feats, n_feats, kernel_size))
 
         # define tail module
         m_tail = [
             Upsampler(conv, scale, n_feats, act=False),
-            conv(n_feats, args['n_colors'], kernel_size)
+            conv(n_feats, args["n_colors"], kernel_size),
         ]
 
         self.head = nn.Sequential(*m_head)
@@ -149,7 +174,7 @@ class EDSR(nn.Module):
         x = self.tail(res)
         x = self.add_mean(x)
 
-        return x 
+        return x
 
     def load_state_dict(self, state_dict, strict=True):
         own_state = self.state_dict()
@@ -160,30 +185,33 @@ class EDSR(nn.Module):
                 try:
                     own_state[name].copy_(param)
                 except Exception:
-                    if name.find('tail') == -1:
-                        raise RuntimeError('While copying the parameter named {}, '
-                                           'whose dimensions in the model are {} and '
-                                           'whose dimensions in the checkpoint are {}.'
-                                           .format(name, own_state[name].size(), param.size()))
+                    if name.find("tail") == -1:
+                        raise RuntimeError(
+                            "While copying the parameter named {}, "
+                            "whose dimensions in the model are {} and "
+                            "whose dimensions in the checkpoint are {}.".format(
+                                name, own_state[name].size(), param.size()
+                            )
+                        )
             elif strict:
-                if name.find('tail') == -1:
-                    raise KeyError('unexpected key "{}" in state_dict'
-                                   .format(name))
+                if name.find("tail") == -1:
+                    raise KeyError('unexpected key "{}" in state_dict'.format(name))
 
-def load(model, pre_train='download', resume=0, cpu=False):
-        load_from = None
-        kwargs = {}
-        if pre_train == 'download':
-            print('Download the model')
-            # dir_model = os.path.join('..', 'models')
-            #os.makedirs(dir_model, exist_ok=True)
-            load_from = torch.utils.model_zoo.load_url(
-                model.url,
-                #model_dir=dir_model,
-                **kwargs
-            )
-        elif pre_train:
-            print('Load the model from {}'.format(pre_train))
-            load_from = torch.load(pre_train, **kwargs)
-        if load_from:
-            model.load_state_dict(load_from, strict=False)
+
+def load(model, pre_train="download", resume=0, cpu=False):
+    load_from = None
+    kwargs = {}
+    if pre_train == "download":
+        print("Download the model")
+        # dir_model = os.path.join('..', 'models')
+        # os.makedirs(dir_model, exist_ok=True)
+        load_from = torch.utils.model_zoo.load_url(
+            model.url,
+            # model_dir=dir_model,
+            **kwargs
+        )
+    elif pre_train:
+        print("Load the model from {}".format(pre_train))
+        load_from = torch.load(pre_train, **kwargs)
+    if load_from:
+        model.load_state_dict(load_from, strict=False)
--- main.py
+++ main.py
@@ -8,129 +8,136 @@ import copy
 from tqdm import tqdm
 from EDSR import make_model, load
 
-device = 'cuda'
-def loadEDSR(n_resblocks = 16, n_feats=64, scale = [4]): 
-  args = {'n_resblocks' : n_resblocks,
-          'n_feats' : n_feats,
-          'scale' : scale,
-          'rgb_range' : 255,
-          'n_colors' : 3,
-          'res_scale' : 1,
-          }
+device = "cuda"
 
-  model = make_model(args).to(device)
-  load(model)
-  dir(model)
-  return model
+
+def loadEDSR(n_resblocks=16, n_feats=64, scale=[4]):
+    args = {
+        "n_resblocks": n_resblocks,
+        "n_feats": n_feats,
+        "scale": scale,
+        "rgb_range": 255,
+        "n_colors": 3,
+        "res_scale": 1,
+    }
+    """
+    """
+    model = make_model(args).to(device)
+    load(model)
+    dir(model)
+    return model
+
 
 def randomImage(dimension):
-    B = np.random.random((dimension,dimension))*255.
+    B = np.random.random((dimension, dimension)) * 255.0
     B = torch.tensor(B)
     B.unsqueeze_(0)
     B = B.repeat(3, 1, 1)
     B = B.unsqueeze(0)
     return B.float()
 
+
 def clearCuda(inputImage, output_image):
-  output_image=output_image.cpu()
-  inputImage = inputImage.cpu()
-  del inputImage
-  del output_image
-  gc.collect()
-  torch.cuda.empty_cache()
+    output_image = output_image.cpu()
+    inputImage = inputImage.cpu()
+    del inputImage
+    del output_image
+    gc.collect()
+    torch.cuda.empty_cache()
+
 
 model = loadEDSR()
 
-#part 1
+# part 1
 print()
-print('Getting maximum unacceptable dimension which is a power of two...')
+print("Getting maximum unacceptable dimension which is a power of two...")
 result1 = {}
 lastDimension = 0
 dimension = 2
-while(True):
-  input_image = randomImage(dimension)
-  input_image = input_image.to(device)
-  print('Testing dimension: {0}x{1} ...'.format(dimension,dimension))
-  with torch.no_grad():
-    try:
-      start = time.time()
-      output_image = model(input_image)
-      end = time.time()
-      total_time = end - start
-      if dimension in result1.keys():
-        result1[dimension].append(total_time)
-      else:
-        result1[dimension] = [total_time]
-      print('Dimension ok.')
-      dimension *= 2
-      clearCuda(input_image, output_image)
-    except RuntimeError as e:
-      print('Dimension NOT OK!')
-      print('-----------------------------------------------------------')
-      print(e)
-      if dimension in result1.keys():
-        result1[dimension].append(math.inf)
-      else:
-        result1[dimension] = [math.inf]
-      lastDimension = dimension
-      clearCuda(input_image, output_image)
-      break
+while True:
+    input_image = randomImage(dimension)
+    input_image = input_image.to(device)
+    print("Testing dimension: {0}x{1} ...".format(dimension, dimension))
+    with torch.no_grad():
+        try:
+            start = time.time()
+            output_image = model(input_image)
+            end = time.time()
+            total_time = end - start
+            if dimension in result1.keys():
+                result1[dimension].append(total_time)
+            else:
+                result1[dimension] = [total_time]
+            print("Dimension ok.")
+            dimension *= 2
+            clearCuda(input_image, output_image)
+        except RuntimeError as e:
+            print("Dimension NOT OK!")
+            print("-----------------------------------------------------------")
+            print(e)
+            if dimension in result1.keys():
+                result1[dimension].append(math.inf)
+            else:
+                result1[dimension] = [math.inf]
+            lastDimension = dimension
+            clearCuda(input_image, output_image)
+            break
 
 for k, v in result1.items():
-  result1[k] = sum(v)/float(len(v)) 
+    result1[k] = sum(v) / float(len(v))
 
 
 # part 2
 print()
-print('Getting maximum acceptable dimension...')
+print("Getting maximum acceptable dimension...")
 result2 = {}
 dimension = 1024
 maxm = math.inf
 minm = -math.inf
 last = 0
-while(True):
-  input_image = randomImage(dimension)
-  input_image = input_image.to(device)
-  print('Testing dimension: {0}x{1} ...'.format(dimension,dimension))
-  with torch.no_grad():
-    try:
-      if last == dimension:
-        break
-      start = time.time()
-      output_image = model(input_image)
-      end = time.time()
-      total_time = end - start
-      last = dimension
-      if dimension in result2.keys():
-        result2[dimension].append(total_time)
-      else:
-        result2[dimension] = [total_time]
-      minm = copy.copy(dimension)
-      print('Dimension ok.')
-      if maxm == math.inf:
-        dimension *= 2
-      else:
-        dimension = dimension + (maxm-minm)//2
-      clearCuda(input_image, output_image)
-    except RuntimeError as e:
-      print('Dimension NOT OK!')
-      print(e)
-      print('-----------------------------------------------------------')
-      maxm = copy.copy(dimension)
-      if dimension in result2.keys():
-        result2[dimension].append(math.inf)
-      else:
-        result2[dimension] = [math.inf]
-      if minm == -math.inf:
-        dimension = dimension//2
-      else:
-        dimension = minm + (maxm-minm)//2
-      clearCuda(input_image, output_image)
-      continue
+while True:
+    input_image = randomImage(dimension)
+    input_image = input_image.to(device)
+    print("Testing dimension: {0}x{1} ...".format(dimension, dimension))
+    with torch.no_grad():
+        try:
+            if last == dimension:
+                break
+            start = time.time()
+            output_image = model(input_image)
+            end = time.time()
+            total_time = end - start
+            last = dimension
+            if dimension in result2.keys():
+                result2[dimension].append(total_time)
+            else:
+                result2[dimension] = [total_time]
+            minm = copy.copy(dimension)
+            print("Dimension ok.")
+            if maxm == math.inf:
+                dimension *= 2
+            else:
+                dimension = dimension + (maxm - minm) // 2
+            clearCuda(input_image, output_image)
+        except RuntimeError as e:
+            print("Dimension NOT OK!")
+            print(e)
+            print("-----------------------------------------------------------")
+            maxm = copy.copy(dimension)
+            if dimension in result2.keys():
+                result2[dimension].append(math.inf)
+            else:
+                result2[dimension] = [math.inf]
+            if minm == -math.inf:
+                dimension = dimension // 2
+            else:
+                dimension = minm + (maxm - minm) // 2
+            clearCuda(input_image, output_image)
+            continue
 
 for k, v in result2.items():
-  result2[k] = sum(v)/float(len(v))
-# print(result2) 
+    result2[k] = sum(v) / float(len(v))
+# print(result2)
 
 dic_items = result2.items()
 result2 = sorted(dic_items)
@@ -140,95 +147,108 @@ result2 = dict(result2)
 z = {**result1, **result2}
 final = {}
 for key, value in z.items():
-  if value == math.inf:
-    continue
-  final[key] = value
+    if value == math.inf:
+        continue
+    final[key] = value
 
-print('\nValid Results: ')
+print("\nValid Results: ")
 print(final)
 
-print('\nPlotting detailed results...')
+print("\nPlotting detailed results...")
 date = "_".join(str(time.ctime()).split())
-date = "_".join(date.split(':'))
-filename = 'dim2n_vs_time' + "_" + date
+date = "_".join(date.split(":"))
+filename = "dim2n_vs_time" + "_" + date
 
 r = sorted(final.items())
 
 x, y = zip(*r)
-plt.xlabel('Dimension (2^n)')
-plt.ylabel('Average time')
+plt.xlabel("Dimension (2^n)")
+plt.ylabel("Average time")
 plt.plot(x, y)
 plt.savefig("figures/{}.png".format(filename))
 plt.show()
 
 # part 3
 print_every = 50
-print('\nPreparing detailed data... ')
+print("\nPreparing detailed data... ")
 result3 = {}
 for i in range(10):
-  print('Run: ', i+1)
-  for d in tqdm(range(1, last+1)):
-    dimension = d
-    input_image = randomImage(dimension)
-    input_image = input_image.to(device)
-    # if(d%print_every == 0):
-      # print('Testing dimension: {0}x{1} ...'.format(dimension,dimension))
-    with torch.no_grad():
-      try:
-        start = time.time()
-        output_image = model(input_image)
-        end = time.time()
-        total_time = end - start
-        if dimension in result3.keys():
-          result3[dimension].append(total_time)
-        else:
-          result3[dimension] = [total_time]
+    print("Run: ", i + 1)
+    for d in tqdm(range(1, last + 1)):
+        dimension = d
+        input_image = randomImage(dimension)
+        input_image = input_image.to(device)
         # if(d%print_every == 0):
-          # print('Dimension ok.')
-        clearCuda(input_image, output_image)
-      except RuntimeError as e:
-        # print('Dimension NOT OK!')
-        # print('-----------------------------------------------------------')
-        print(e)
-        if dimension in result3.keys():
-          result3[dimension].append(math.inf)
-        else:
-          result3[dimension] = [math.inf]
-        clearCuda(input_image, output_image)
-        break
+        # print('Testing dimension: {0}x{1} ...'.format(dimension,dimension))
+        with torch.no_grad():
+            try:
+                start = time.time()
+                output_image = model(input_image)
+                end = time.time()
+                total_time = end - start
+                if dimension in result3.keys():
+                    result3[dimension].append(total_time)
+                else:
+                    result3[dimension] = [total_time]
+                # if(d%print_every == 0):
+                # print('Dimension ok.')
+                clearCuda(input_image, output_image)
+            except RuntimeError as e:
+                # print('Dimension NOT OK!')
+                # print('-----------------------------------------------------------')
+                print(e)
+                if dimension in result3.keys():
+                    result3[dimension].append(math.inf)
+                else:
+                    result3[dimension] = [math.inf]
+                clearCuda(input_image, output_image)
+                break
 
 mean_dict = {}
 std_dict = {}
 
 for k, v in result3.items():
-  mean_dict[k] = np.mean(np.array(v))
-print(mean_dict) 
+    mean_dict[k] = np.mean(np.array(v))
+print(mean_dict)
 
 for k, v in result3.items():
-  std_dict[k] = np.std(np.array(v))
-print(std_dict) 
+    std_dict[k] = np.std(np.array(v))
+print(std_dict)
 
-print('Plotting detailed results...')
+print("Plotting detailed results...")
 date = "_".join(str(time.ctime()).split())
-date = "_".join(date.split(':'))
-filename = 'dim_vs_meantime' + "_" + date
+date = "_".join(date.split(":"))
+filename = "dim_vs_meantime" + "_" + date
 
 mean_dict = sorted(mean_dict.items())
 x, y = zip(*mean_dict)
-plt.xlabel('Dimension')
-plt.ylabel('Mean time (10 runs)')
+plt.xlabel("Dimension")
+plt.ylabel("Mean time (10 runs)")
 plt.plot(x, y)
 plt.savefig("figures/{}.png".format(filename))
 plt.show()
 
 date = "_".join(str(time.ctime()).split())
-date = "_".join(date.split(':'))
-filename = 'dim_vs_stdtime' + "_" + date
+date = "_".join(date.split(":"))
+filename = "dim_vs_stdtime" + "_" + date
 
 std_dict = sorted(std_dict.items())
 x, y = zip(*std_dict)
-plt.xlabel('Dimension')
-plt.ylabel('Std of time (10 runs)')
+plt.xlabel("Dimension")
+plt.ylabel("Std of time (10 runs)")
 plt.plot(x, y)
 plt.savefig("figures/{}.png".format(filename))
 plt.show()
+
+def main():
+    # load model
+    # get the highest unacceptable dimension which is a power of 2
+    # get the maximum acceptable dimension
+    # get detailed result
+    # get mean
+    # get std
+    # plot data
+    pass
+    
+if __name__ == "__main__":
+    main()
