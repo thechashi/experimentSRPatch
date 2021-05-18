f2a8599e1f0500c44c74a0c3cd8a3c0c7d7cedb0
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-05-18 15:38:19 -0400

    fix: install.sh fixed

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 python3 -m pip install numpy
 python3 -m pip install matplotlib
 python3 -m pip install tqdm
-pip3 install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+python3 -m pip install torch torchvision torchaudio
 ptyhon3 -m pip install pynvml
 python3 -m pip install pandas
