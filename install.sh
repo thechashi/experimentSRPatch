39fddd23210b06904d7cff25abe05ce194e81b6b
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-05-21 13:41:05 -0400

    refactor: spacing in logs

--- install.sh
+++
@@ -1,6 +0,0 @@
-python3 -m pip install numpy
-python3 -m pip install matplotlib
-python3 -m pip install tqdm
-python3 -m pip install torch torchvision torchaudio
-ptyhon3 -m pip install pynvml
-python3 -m pip install pandas
