decab789132503446bae136e89a237015b8aec89
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-06-08 22:54:14 -0400

    feat: forward chop iterative ready

--- experimentsrpatch/forward_chop.py
+++ experimentsrpatch/forward_chop.py
@@ -2,49 +2,56 @@ import math
 import numpy as np
 import utilities as ut
 import modelloader as md
-import subprocess
-from PIL import Image
 import torch
 import torchvision
 import matplotlib.pyplot as plt
-def forward_chop_iterative(x, model = None, shave=10, min_size=6400):
+from tqdm import tqdm
+import time
+import sys
+def forward_chop_iterative(x, model = None, shave=12, min_size=1024):
         dim = round(math.sqrt(min_size))
         b, c, h, w = x.size()
-# =============================================================================
-#         print('type', type(x))
-#         print('cuda:', x.is_cuda)
-#         print()
-# =============================================================================
         device = 'cuda'
         count = 0
         output = torch.tensor(np.zeros((b, c, h*4, w*4)))
-# =============================================================================
-#         print('input_size: ', x.size())
-#         print('output_size: ', output.shape)
-# =============================================================================
-        
+        total_time = 0
         new_i_s = 0
-        for i in range(0, h, dim-2*shave):
+        for i in tqdm(range(0, h, dim-2*shave)):
             new_j_s = 0
             new_j_e = 0
-            print('Loop: ', count)
-            print('Before:')
-            subprocess.run("gpustat", shell=True)
+# =============================================================================
+#             subprocess.run("gpustat", shell=True)
+# =============================================================================
             for j in range(0, w, dim-2*shave):
+# =============================================================================
+#                 print(i,j)
+#                 subprocess.run("gpustat", shell=True)
+# =============================================================================
                 count += 1
                 h_s = i
                 h_e = min(h, i+dim)
                 w_s = j
                 w_e = min(w, j+dim)
                 lr = x[:, :, h_s:h_e, w_s:w_e]
-                lr = lr.to(device)
-                sr = model(lr)
-                sr = sr.to('cpu')
-                #sr = sr.detach().numpy()
 # =============================================================================
 #                 print('h: {}x{} w: {}x{}'.format(h_s, h_e, w_s, w_e))
 #                 print('current dim: {}x{}'.format(h_e-h_s,w_e-w_s))
 # =============================================================================
+                with torch.no_grad():
+                    lr = lr.to(device)
+# =============================================================================
+#                     subprocess.run("gpustat", shell=True)
+# =============================================================================
+                    start = time.time()
+                    sr = model(lr)
+                    end = time.time()
+                    processing_time = end - start
+                    total_time += processing_time
+                    sr = sr.to('cpu')
+# =============================================================================
+#                     subprocess.run("gpustat", shell=True)
+# =============================================================================
+                #sr = sr.detach().numpy()
                 n_h = (h_e-h_s)*4
                 n_w = (w_e-w_s)*4
 
@@ -85,9 +92,15 @@ def forward_chop_iterative(x, model = None, shave=10, min_size=6400):
                     break
                 new_j_s = new_j_e
                 ut.clear_cuda(lr, None)
-            print('After:')
-            subprocess.run("gpustat", shell=True)
-            print()
+# =============================================================================
+#                 subprocess.run("gpustat", shell=True)
+#                 print('-----------------------------------------------------')
+# =============================================================================
+# =============================================================================
+#             print('After:')
+#             subprocess.run("gpustat", shell=True)
+#             print()
+# =============================================================================
             new_i_s = new_i_e
             if h_e == h:
                 break
@@ -95,23 +108,33 @@ def forward_chop_iterative(x, model = None, shave=10, min_size=6400):
 #         print(count)
 #         print(output.shape)
 # =============================================================================
+        print('Patch dimension: {}x{}'.format(dim, dim))
+        print('Total pacthes: ', count)
+        print('Total EDSR Processing time: ', total_time)
         return output
 
 if __name__ == "__main__":
-    dimension = 32
+    
+    img_path = sys.argv[1] if len(sys.argv) > 1 else 'test2.jpg'
+    dimension = int(sys.argv[2])if len(sys.argv) > 2 else 32
+    shave = int(sys.argv[3]) if len(sys.argv) > 3 else 12
+    
     device = 'cuda'
-    img = torchvision.io.read_image('test2.jpg')
+    img = torchvision.io.read_image(img_path)
     
     c, h, w = img.shape
     img = img.reshape((1,c, h, w))
     plt.imshow(img[0].permute((1,2,0)))
     input_image = img.float()
 
-# =============================================================================
-#     model = md.load_edsr(device=device)
-#     out = forward_chop_iterative(input_image, model = model)
-#     out = out.int()
-#     
-#     plt.imshow(out[0].permute((1,2,0)))
-# =============================================================================
+    model = md.load_edsr(device=device)
+    st = time.time()
+    out = forward_chop_iterative(input_image, shave=shave, min_size=dimension*dimension, model = model)
+    et = time.time()
+    tt = et - st
+    print('Total forward chopping time: ', tt)
+    out = out.int()
+    
+    plt.imshow(out[0].permute((1,2,0)))
+    plt.savefig('result_imagex4.png', bbox_inches='tight')
 
