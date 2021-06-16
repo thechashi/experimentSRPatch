20a57875d0673b0e68ac35630c94ae7ba54b0356
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-06-16 00:36:21 -0400

    feat: forward chop iterative with multiple patch dimension ready

--- experimentsrpatch/check_forward_chop.py
+++ experimentsrpatch/check_forward_chop.py
@@ -1,22 +1,45 @@
 import sys
 import subprocess
-import tqdm
+from tqdm import tqdm
 import pandas as pd
 import numpy as np
+import time
+import utilities as ut
 import matplotlib.pyplot as plt
 
 
-def check_differnet_patches_in_forward_chop(min_dim, max_dim, shave, image_path):
+def check_differnet_patches_in_forward_chop(min_dim, max_dim, shave, image_path, run = 1, device="cuda"):
+    
+    model_name = "EDSR"
+    device = device
+    
+    _, device_name = ut.get_device_details()
+    total_memory, _, _ = ut.get_gpu_details(
+        device, "\nDevice info:", logger=None, print_details=False
+    )
+    print_result = "0" 
     full_result = []
-    for d in range(min_dim, max_dim+1):
-        print(d)
+    for d in tqdm(range(min_dim, max_dim+1)):
         s = [d]
-        p = subprocess.run("python3 forward_chop.py test2.jpg 40 10 0", shell=True, capture_output = True)
-        s += list(map(float, p.stdout.decode().split()[1:]))
-        full_result.append(s)
+        command = "python3 " + "forward_chop.py " + image_path + " " +   str(d) + " "  + str(shave) + " " +  print_result
+        p = subprocess.run(command, shell=True, capture_output = True)
+        if p.returncode == 0:
+            s += list(map(float, p.stdout.decode().split()[1:]))
+            full_result.append(s)
+        else:
+            break
+        
     full_result = pd.DataFrame(full_result)
     full_result.columns = ['Dimension', 'EDSR Processing Time', 'Cropping time', 'Shifting Time', 'CUDA Cleanign Time']
-    plt_title = 'EDSR Processing time'
+    
+    if device =="cuda":
+        plt_title = 'Model: {} | GPU: {} | Memory: {} MB'.format(model_name, device_name, total_memory)
+    else:
+        plt_title = 'Model: {} | Device: {}'.format(model_name, 'CPU')
+    
+    date = "_".join(str(time.ctime()).split())
+    date = "_".join(date.split(":"))
+    
     x_data, y_data = (
         np.array(full_result.iloc[:, 0].values).flatten(),
         np.array(full_result.iloc[:, 1].values).flatten(),
@@ -27,7 +50,7 @@ def check_differnet_patches_in_forward_chop(min_dim, max_dim, shave, image_path)
     plt.ylabel(y_label)
     plt.title(plt_title)
     plt.plot(x_data, y_data)
-    #plt.savefig("results/{0}/{1}.png".format(foldername, filename))
+    plt.savefig("results/forward_chop_experiment/{0}.png".format('processing_time_'+date))
     plt.show()
     
     x_data, y_data = (
@@ -40,7 +63,7 @@ def check_differnet_patches_in_forward_chop(min_dim, max_dim, shave, image_path)
     plt.ylabel(y_label)
     plt.title(plt_title)
     plt.plot(x_data, y_data)
-    #plt.savefig("results/{0}/{1}.png".format(foldername, filename))
+    plt.savefig("results/forward_chop_experiment/{0}.png".format('cropping_time_'+date))
     plt.show()
     
     x_data, y_data = (
@@ -53,7 +76,7 @@ def check_differnet_patches_in_forward_chop(min_dim, max_dim, shave, image_path)
     plt.ylabel(y_label)
     plt.title(plt_title)
     plt.plot(x_data, y_data)
-    #plt.savefig("results/{0}/{1}.png".format(foldername, filename))
+    plt.savefig("results/forward_chop_experiment/{0}.png".format('shfiting_time_'+date))
     plt.show()
     
     x_data, y_data = (
@@ -66,10 +89,24 @@ def check_differnet_patches_in_forward_chop(min_dim, max_dim, shave, image_path)
     plt.ylabel(y_label)
     plt.title(plt_title)
     plt.plot(x_data, y_data)
-    #plt.savefig("results/{0}/{1}.png".format(foldername, filename))
+    plt.savefig("results/forward_chop_experiment/{0}.png".format('cuda_cleaning_time_'+date))
     plt.show()
+    
+    filename = "stat_" + 'EDSR_forward_processing_iterative_' + date
+    file = open("results/" + 'forward_chop_experiment' + "/" + filename, "a")
+    file.write(device+'\n')
+    file.write(device_name+'\n')
+    file.write('Memory: ' + str(total_memory) + 'MB\n')
+    full_result.to_csv(file)
+    file.close()
 
 
         
 if __name__ == "__main__":
-    check_differnet_patches_in_forward_chop(40,100, 10, 'test2.jpg')
+    start_dim = int(sys.argv[1])
+    end_dim = int(sys.argv[2])
+    shave = int(sys.argv[3])
+    image_path = sys.argv[4]
+    run = int(sys.argv[5]) if len(sys.argv) > 5 else 2
+    device = sys.argv[6] if len(sys.argv) > 6 else "cuda"
+    check_differnet_patches_in_forward_chop(start_dim, end_dim, shave, image_path, run, device)
