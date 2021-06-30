8b995281b9222899971939ab8dc3467652c8e526
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-06-30 12:31:11 -0400

    rrdb model loading from saved models

---
+++ experimentsrpatch/downloader.py
@@ -0,0 +1,70 @@
+"""
+Downloader module to fetch the image zip files from the toml
+"""
+import os
+import tarfile
+import zipfile
+from pathlib import Path
+
+import toml
+
+
+def get_file_name(url):
+    """
+
+    :param url: file url
+    :return: file_name, file_ext, odir
+    """
+    base_name = os.path.basename(url)
+    base_name = str.split(base_name, sep="?")[0]
+    file_name, file_ext = (
+        str.split(base_name, sep=".")[0],
+        ".".join(str.split(base_name, sep=".")[1:]),
+    )
+
+    return file_name, file_ext
+
+
+def file_extraction(file_tmp, file_ext, odir):
+    """
+
+    :param file_tmp: the downloaded file
+    :param file_ext: the extension of the file
+    :param odir: the output directory of the file
+    :return:
+    """
+    odir = os.path.abspath(odir)
+    if not os.path.isdir(odir):
+        os.makedirs(odir)
+    if file_ext == "zip":
+        with zipfile.ZipFile(file_tmp) as zip_ref:
+            zip_ref.extractall(odir)
+    if file_ext in ("tar", "tar.gz", "tar.xz"):
+        tar = tarfile.open(file_tmp)
+        tar.extractall(odir)
+
+
+def data_download(datadict, file_down):
+    """
+
+    :param datadict: the dictionary that contains the download links
+    :param odir: the path where the files will be extracted
+    :return:
+    """
+    down_path = Path(file_down)
+    if not os.path.isdir(down_path):
+        os.mkdir(down_path)
+
+    for _, url_key in enumerate(datadict):
+        url = datadict[url_key][0]
+        odir = datadict[url_key][1]
+        file_name, file_ext = get_file_name(url)
+        file_down_path = down_path / (file_name + "." + file_ext)
+        if not file_down_path.exists():
+            os.system(f"wget {url} -O {file_down_path}")
+            file_extraction(file_down_path, file_ext, odir)
+        if url_key == "Medical_3D":
+            files = os.listdir(odir / "Medical_3D")
+            for compressed_file in files:
+                file_extraction(odir / "Medical_3D" / compressed_file, file_ext, odir)
+                os.remove(odir / "Medical_3D" / compressed_file)
--- experimentsrpatch/modelloader.py
+++ experimentsrpatch/modelloader.py
@@ -33,14 +33,31 @@ def load_edsr(device, n_resblocks=16, n_feats=64, model_details=True):
     #     elif cpu == 'False':
     #         print('\nCUDA Available\n')
     # =============================================================================
+# =============================================================================
+#     args = {
+#         "n_resblocks": n_resblocks,
+#         "n_feats": n_feats,
+#         "scale": [scale],
+#         "rgb_range": 255,
+#         "n_colors": 3,
+#         "res_scale": 1,
+#         "cpu": cpu,
+#     }
+# =============================================================================
     args = {
-        "n_resblocks": n_resblocks,
-        "n_feats": n_feats,
-        "scale": [scale],
-        "rgb_range": 255,
-        "n_colors": 3,
-        "res_scale": 1,
-        "cpu": cpu,
+     "G0":64, "RDNconfig":'B', "RDNkSize":3, "act":'relu', "batch_size":16,
+     "betas":(0.9, 0.999), "chop":True, "cpu":True,
+     "data_range":'1-800/801-810', "data_test":['Demo'], "data_train":['DIV2K'],
+     "debug":False, "decay":'200', "dilation":False, "dir_data":'../../../dataset',
+     "dir_demo":'../test', "epochs":300, "epsilon":1e-08, "ext":'sep', "extend":'.',
+     "gamma":0.5, "gan_k":1, "gclip":0, "load":'', "loss":'1*L1', "lr":0.0001, "model":'EDSR',
+     "momentum":0.9, "n_GPUs":1, "n_colors":3, "n_feats":64, "n_resblocks":16, "n_resgroups":10,
+     "n_threads":6, "no_augment":False, "optimizer":'ADAM', "patch_size":192,
+     "pre_train":'download', "precision":'single', "print_every":100, "reduction":16,
+     "res_scale":1, "reset":False, "resume":0, "rgb_range":255, "save":'test', "save_gt":False,
+     "save_models":False, "save_results":True, "scale":[4], "seed":1, "self_ensemble":False,
+     "shift_mean":True, "skip_threshold":100000000.0, "split_batch":1, "template":'.',
+     "test_every":1000, "test_only":True, "weight_decay":0
     }
     model = edsr.make_model(args).to(device)
     edsr.load(model)
@@ -62,5 +79,6 @@ def load_rrdb(device):
         config["generator"]["num_out_channels"],
         config["generator"]["num_fea"],
         config["generator"]["num_layers"])
-    rrdb.load(model, path).to(device)
+    model = model.to(device)
+    rrdb.load(model, path)
     return model
