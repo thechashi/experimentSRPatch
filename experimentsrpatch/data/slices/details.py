44e574c42cab01a8c89083f4fc37bf712a0186b4
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-07-06 16:37:23 -0400

    chore: larger npz file added

--- experimentsrpatch/data/slices/details.py
+++ experimentsrpatch/data/slices/details.py
@@ -1,10 +1,18 @@
+import numpy as np
 import experimentsrpatch.utilities as ut
 
+def create_custom_npz(input_file, height, width):
+    image = np.load(input_file)
+    image = image.f.arr_0
+    image = np.resize(image, (height, width))
+    np.savez("10.npz", image)
 
-for i in range(10):
-    name = str(i) + ".npz"
-    image = ut.npz_loader(name)
-    c, h, w = image.shape
-    print("Name: ", name)
-    print("C, H, W: ", c, h , w)
-    
+if __name__ == "__main__":
+    create_custom_npz("0.npz", 4000, 2300)
+    for i in range(11):
+        name = str(i) + ".npz"
+        image = ut.npz_loader(name)
+        c, h, w = image.shape
+        print("Name: ", name)
+        print("C, H, W: ", c, h , w)
+        
--- experimentsrpatch/helper_batch_patch_forward_chop.py
+++ experimentsrpatch/helper_batch_patch_forward_chop.py
@@ -6,20 +6,21 @@ import batch_patch_forward_chop as bpfc
 import utilities as ut
 
 if __name__ == "__main__":
-    img_path = sys.argv[1] if len(sys.argv) > 1 else "data/test2.jpg"
-    dimension = int(sys.argv[2]) if len(sys.argv) > 2 else 45
+    img_path = sys.argv[1] if len(sys.argv) > 1 else "data/slices/10.npz"
+    dimension = int(sys.argv[2]) if len(sys.argv) > 2 else 145
     shave = int(sys.argv[3]) if len(sys.argv) > 3 else 12
     batch_size = int(sys.argv[4]) if len(sys.argv) > 4 else 1
     scale = int(sys.argv[5]) if len(sys.argv) > 5 else 4
     print_result = bool(int(sys.argv[6])) if len(sys.argv) > 6 else False
     device = str(sys.argv[7]) if len(sys.argv) > 7 else "cuda"
-    model_name = str(sys.argv[8]) if len(sys.argv) > 8 else "EDSR"
+    model_name = str(sys.argv[8]) if len(sys.argv) > 8 else "RRDB"
     
     if model_name == "EDSR":
         input_image = ut.load_image(img_path)
     elif model_name == "RRDB":
         #input_image = ut.load_grayscale_image(img_path)
         input_image = ut.npz_loader(img_path)
+        print(input_image.shape)
     else:
         raise Exception('{} : Unknown model...'.format(model_name))
         
