56a16fe4e535d36ce4ccd36ac044196cee9ca4d1
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-07-08 00:53:06 -0400

    feat: npy plotter added

--- experimentsrpatch/results/npy_opener.py
+++ experimentsrpatch/results/npy_opener.py
@@ -3,3 +3,9 @@ data = np.load('outputx4.npy')
 print("The image is: ")
 print(data)
 print('Output data type: ', type(data.dtype))
+
+from matplotlib import pyplot as plt
+
+plt.imshow(data[0,:,:], cmap='gray')
+plt.show()
+
