3ccfb3b520ffc44cf86b7216d1e83d5cf856d378
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-07-15 13:00:09 -0400

    fix: batch_experiment.py 10/10

--- experimentsrpatch/batch_experiment.py
+++ experimentsrpatch/batch_experiment.py
@@ -1,12 +1,24 @@
+"""
+Helper file for running batch experiment with resuming option.
+"""
 import subprocess
 import toml
+
 if __name__ == "__main__":
-    loader_config = toml.load('../loader_config.toml')
+    loader_config = toml.load("../loader_config.toml")
     if loader_config["batch_range_experiment"]["status"] == "ongoing":
-        subprocess.run("gpustat", shell=True)
-        subprocess.run("time python3 batch_forward_chop_experiment.py batch_range", shell= True)
+        subprocess.run("gpustat", shell=True, check=True)
+        subprocess.run(
+            "time python3 batch_forward_chop_experiment.py batch_range",
+            shell=True,
+            check=True,
+        )
     else:
-        subprocess.run("gpustat", shell=True)
-        p = subprocess.run("time python3 binarysearch.py", shell=True)
-        subprocess.run("gpustat", shell=True)
-        p = subprocess.run("time python3 batch_forward_chop_experiment.py batch_range", shell= True)
+        subprocess.run("gpustat", shell=True, check=True)
+        p = subprocess.run("time python3 binarysearch.py", shell=True, check=True)
+        subprocess.run("gpustat", shell=True, check=True)
+        p = subprocess.run(
+            "time python3 batch_forward_chop_experiment.py batch_range",
+            shell=True,
+            check=True,
+        )
