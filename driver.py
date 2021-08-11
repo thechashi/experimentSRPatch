747ae1f2694271512838db08a44615f92cdedf40
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-08-10 22:44:10 -0400

    fix: rrdb downloading link updated

---
+++ driver.py
@@ -0,0 +1,9 @@
+import experimentsrpatch.binarysearch as bs
+import experimentsrpatch.utilities as ut
+import toml 
+import sys
+if __name__ == "__main__":
+    sys.excepthook = ut.exception_handler
+
+    config = toml.load("batch_processing.toml")
+    bs.do_binary_search(config["model"])
--- tests/test_binarysearch.py
+++ tests/test_binarysearch.py
@@ -1,7 +1,10 @@
-import experimentsrpatch.binarysearch
+import experimentsrpatch.binarysearch as bs
+import experimentsrpatch.utilities as ut
 
 def test_maximum_acceptable_dimension():
-    assert True
+    result = bs.maximum_unacceptable_dimension_2n(device="cuda", logger=ut.get_logger(), start_dim=200, model_name="RRDB")
+    assert type(result) == int
+    assert result >= 200
 
 def test_maximum_unacceptable_dimension():
     assert True
