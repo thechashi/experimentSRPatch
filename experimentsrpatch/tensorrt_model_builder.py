d509d44a7e87ba91a99552ccadf271e523047ddf
chashi-mahiul-islam-bd <ci20l@my.fsu.edu>
2021-08-27 10:32:37 -0400

    fix: edsr tensorrt error

--- experimentsrpatch/tensorrt_model_builder.py
+++ experimentsrpatch/tensorrt_model_builder.py
@@ -1,10 +1,8 @@
+import torch
+import torch.onnx
 import modelloader as md
+import utilities as ut
 
 model = md.load_edsr(device="cuda")
-
-import torch  
-
-dummy_input = torch.randn(3, 3, 1, 1, device='cuda')
-
-import torch.onnx
+dummy_input = ut.random_image(128).cuda()
 torch.onnx.export(model, dummy_input, "edsr.onnx", verbose=False)
--- misc/tensorrt/temp.py
+++ misc/tensorrt/temp.py
@@ -1,20 +1,24 @@
 from torchvision import models
+
 model = models.resnet50(pretrained=True)
 
 import cv2
 import torch
 from albumentations import Resize, Compose
-from albumentations.pytorch.transforms import  ToTensorV2
+from albumentations.pytorch.transforms import ToTensorV2
 from albumentations.augmentations.transforms import Normalize
+
 
 def preprocess_image(img_path):
     # transformations for the input data
-    transforms = Compose([
-        Resize(224, 224, interpolation=cv2.INTER_NEAREST),
-        Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ToTensorV2(),
-    ])
-    
+    transforms = Compose(
+        [
+            Resize(224, 224, interpolation=cv2.INTER_NEAREST),
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ToTensorV2(),
+        ]
+    )
+
     # read input image
     input_img = cv2.imread(img_path)
     # do transformations
@@ -22,11 +26,13 @@ def preprocess_image(img_path):
     batch_data = torch.unsqueeze(input_data, 0)
     return batch_data
 
+
 input = preprocess_image("turkish_coffee.jpg").cuda()
 
 model.eval()
 model.cuda()
 output = model(input)
+
 
 def postprocess(output_data):
     # get class names
@@ -50,13 +56,20 @@ def postprocess(output_data):
         )
         i += 1
 
+
 postprocess(output)
 
 import onnx
 
-ONNX_FILE_PATH = 'resnet50.onnx'
-torch.onnx.export(model, input, ONNX_FILE_PATH, input_names=['input'],
-                  output_names=['output'], export_params=True)
+ONNX_FILE_PATH = "resnet50.onnx"
+torch.onnx.export(
+    model,
+    input,
+    ONNX_FILE_PATH,
+    input_names=["input"],
+    output_names=["output"],
+    export_params=True,
+)
 onnx_model = onnx.load(ONNX_FILE_PATH)
 onnx.checker.check_model(onnx_model)
 
@@ -68,17 +81,18 @@ import tensorrt as trt
 # logger to capture errors, warnings, and other information during the build and inference phases
 TRT_LOGGER = trt.Logger()
 
+
 def build_engine(onnx_file_path):
     # initialize TensorRT engine and parse ONNX model
     builder = trt.Builder(TRT_LOGGER)
     network = builder.create_network()
     parser = trt.OnnxParser(network, TRT_LOGGER)
-    
+
     # parse ONNX
-    with open(onnx_file_path, 'rb') as model:
-        print('Beginning ONNX file parsing')
+    with open(onnx_file_path, "rb") as model:
+        print("Beginning ONNX file parsing")
         parser.parse(model.read())
-    print('Completed parsing of ONNX file')
+    print("Completed parsing of ONNX file")
 
     # allow TensorRT to use up to 1GB of GPU memory for tactic selection
     builder.max_workspace_size = 1 << 30
@@ -87,40 +101,52 @@ def build_engine(onnx_file_path):
     # use FP16 mode if possible
     if builder.platform_has_fast_fp16:
         builder.fp16_mode = True
-    
+
     # generate TensorRT engine optimized for the target platform
-    print('Building an engine...')
+    print("Building an engine...")
     engine = builder.build_cuda_engine(network)
     context = engine.create_execution_context()
     print("Completed creating Engine")
 
     return engine, context
 
+
 def main():
     # initialize TensorRT engine and parse ONNX model
     engine, context = build_engine(ONNX_FILE_PATH)
-    
+
     # get sizes of input and output and allocate memory required for input data and for output data
     for binding in engine:
         if engine.binding_is_input(binding):  # we expect only one input
             input_shape = engine.get_binding_shape(binding)
-            input_size = trt.volume(input_shape) * engine.max_batch_size * np.dtype(np.float32).itemsize  # in bytes
+            input_size = (
+                trt.volume(input_shape)
+                * engine.max_batch_size
+                * np.dtype(np.float32).itemsize
+            )  # in bytes
             device_input = cuda.mem_alloc(input_size)
         else:  # and one output
             output_shape = engine.get_binding_shape(binding)
             # create page-locked memory buffers (i.e. won't be swapped to disk)
-            host_output = cuda.pagelocked_empty(trt.volume(output_shape) * engine.max_batch_size, dtype=np.float32)
+            host_output = cuda.pagelocked_empty(
+                trt.volume(output_shape) * engine.max_batch_size, dtype=np.float32
+            )
             device_output = cuda.mem_alloc(host_output.nbytes)
-    
+
     # Create a stream in which to copy inputs/outputs and run inference.
     stream = cuda.Stream()
-    
+
     # preprocess input data
-    host_input = np.array(preprocess_image("turkish_coffee.jpg").numpy(), dtype=np.float32, order='C')
+    host_input = np.array(
+        preprocess_image("turkish_coffee.jpg").numpy(), dtype=np.float32, order="C"
+    )
     cuda.memcpy_htod_async(device_input, host_input, stream)
-    
+
     # postprocess results
-    output_data = torch.Tensor(host_output).reshape(engine.max_batch_size, output_shape[0])
+    output_data = torch.Tensor(host_output).reshape(
+        engine.max_batch_size, output_shape[0]
+    )
     postprocess(output_data)
+
 
 main()
