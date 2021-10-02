2204a92bd6d2d75a211e00a41c823c4ab8a177c2
chashi-mahiul-islam-bd <ci20l@my.fsu.edu>
2021-10-02 16:18:21 -0400

    fix: pytorch to onnx cuda issue solved

--- experimentsrpatch/onnx_model_builder.py
+++ experimentsrpatch/onnx_model_builder.py
@@ -46,9 +46,11 @@ def build_onnx_model(model_name, patch_size, onnx_model_name, device="cuda"):
 #         input_image = input_image[:, 2:, :, :]
 #     dummy_input = input_image.to(device)
 # =============================================================================
-    print('Before loading model: ')
-    subprocess.run("gpustat", shell = True)
-    print()
+# =============================================================================
+#     print('Before loading model: ')
+#     subprocess.run("gpustat", shell = True)
+#     print()
+# =============================================================================
     device = ut.get_device_type()
     model = None
     if model_name == "RRDB":
@@ -59,13 +61,17 @@ def build_onnx_model(model_name, patch_size, onnx_model_name, device="cuda"):
         dummy_input = image.unsqueeze(0).to(device)
     elif model_name == "EDSR":
         model = md.load_edsr(device)
-        print('After loading model: ')
-        subprocess.run("gpustat", shell = True)
-        print()
+# =============================================================================
+#         print('After loading model: ')
+#         subprocess.run("gpustat", shell = True)
+#         print()
+# =============================================================================
         dummy_input = ut.random_image(patch_size).to(device)
-        print('After loading image: ')
-        subprocess.run("gpustat", shell = True)
-        print()
+# =============================================================================
+#         print('After loading image: ')
+#         subprocess.run("gpustat", shell = True)
+#         print()
+# =============================================================================
 # =============================================================================
 #         b, c, h, w = 1, 3, patch_size, patch_size
 #         dummy_input = torch.rand(b, c, h, w, requires_grad=False).to(device)
@@ -77,27 +83,34 @@ def build_onnx_model(model_name, patch_size, onnx_model_name, device="cuda"):
     model.eval()
     with torch.no_grad():
 
-        print('Before processing: ')
-        subprocess.run("gpustat", shell = True)
-        print()
+# =============================================================================
+#         print('Before processing: ')
+#         subprocess.run("gpustat", shell = True)
+#         print()
+# =============================================================================
 # =============================================================================
 #         output = model(input_image )
 #         print('After processing: ')
 #         subprocess.run("gpustat", shell = True)
 #         print()
 # =============================================================================
-        torch.onnx.export(
-            model,
-            dummy_input,
-            "inference_models/" + onnx_model_name,
-            verbose=False,
-            opset_version=12,
-            input_names=["input"],
-            output_names=["output"],
-        )
-        print('After processing: ')
-        subprocess.run("gpustat", shell = True)
-        print()
+        try:
+            torch.onnx.export(
+                model,
+                dummy_input,
+                "inference_models/" + onnx_model_name,
+                verbose=False,
+                opset_version=12,
+                input_names=["input"],
+                output_names=["output"],
+            )
+        except RuntimeError as err:
+            sys.exit(1)
+# =============================================================================
+#         print('After processing: ')
+#         subprocess.run("gpustat", shell = True)
+#         print()
+# =============================================================================
         
 if __name__ == "__main__":
     # =============================================================================
--- experimentsrpatch/pytorch_to_trt_pipeline.py
+++ experimentsrpatch/pytorch_to_trt_pipeline.py
@@ -1,7 +1,7 @@
 import click
 import toml
 import subprocess
-
+import utilities as ut
 
 @click.command()
 @click.option("--model_name", default="EDSR", help="Upsampler model name")
@@ -14,21 +14,52 @@ def build_onnx_trt(model_name, patch_dim, use_precision, verbose):
         patch_dim = int(config["max_dim"])
     else:
         patch_dim = int(patch_dim)
+        
+    
     # pytorch to onnx model
     if verbose:
         print("Building ONNX model from the PyTorch model...")
-    onnx_model_name = model_name.lower() + "_" + str(use_precision)+ ".onnx"
+    onnx_model_name = model_name.lower() + "_" + str(use_precision)+ "_" + \
+    str(patch_dim) + ".onnx"
 # =============================================================================
 #     omb.build_onnx_model(model_name, patch_dim, onnx_model_name)
 # =============================================================================
-    command1 = "python3 onnx_model_builder.py " + str(model_name) + " " + \
-        str(patch_dim) + " " + str(onnx_model_name)
-    subprocess.run(command1, shell=True)
+# =============================================================================
+#     command1 = "python3 onnx_model_builder.py " + str(model_name) + " " + \
+#         str(patch_dim) + " " + str(onnx_model_name)
+# =============================================================================
+    command1 = ["python3", "onnx_model_builder.py", str(model_name), str(patch_dim), str(onnx_model_name)]
+    #subprocess.run(command1, shell=True)
+    
+    while True:
+        process_output = subprocess.run(
+            command1,
+            stdout=subprocess.PIPE,
+            text=True,
+        )
+        if process_output.returncode != 0:
+            ut.clear_cuda(None, None)
+            patch_dim -=1
+            print('Memory out. Decreasing patch size. New patch_size = {}'.format(patch_dim))
+# =============================================================================
+#             command1 = "python3 onnx_model_builder.py " + str(model_name) + " " + \
+#                 str(patch_dim) + " " + str(onnx_model_name)
+# =============================================================================
+            command1 = ["python3", "onnx_model_builder.py", str(model_name), str(patch_dim), str(onnx_model_name)]
+        else:
+            ut.clear_cuda(None, None)
+            # for linear search
+            config = toml.load("../config.toml")
+            config["max_dim"] = patch_dim
+            f = open("../config.toml", "w")
+            toml.dump(config, f)
+            break
     
     # onnx to trt
     if verbose:
         print("Building TRT engine from the ONNX model...")
-    trt_model = "inference_models/" + model_name.lower() + "_" + str(use_precision) + "_" + ".trt"
+    trt_model = "inference_models/" + model_name.lower() + "_" + str(use_precision) + \
+        "_" + str(patch_dim) + ".trt"
 # =============================================================================
 #     otu.build_trt_engine("inference_models/"+onnx_model_name, trt_model)
 # =============================================================================
