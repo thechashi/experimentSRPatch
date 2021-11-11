7dc01519b42dc7da17692498a59bb183a670b8ba
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-11-11 13:50:50 -0500

    fix: recursive_forward_chop main function modified

--- experimentsrpatch/forward_chop_recursive.py
+++ experimentsrpatch/forward_chop_recursive.py
@@ -53,11 +53,13 @@ def forward_chop(
             model.eval()
             with torch.no_grad():
                 lr_batch = torch.cat(lr_list[i : (i + n_GPUs)], dim=0)
+                
                 upsampling_time = ut.timer()
                 sr_batch = model(lr_batch)
                 torch.cuda.synchronize()
                 upsampling_time = upsampling_time.toc()
                 timer[1] += upsampling_time
+                
                 sr_list.extend(sr_batch.chunk(n_GPUs, dim=0))
     else:
         sr_list = [
@@ -71,6 +73,7 @@ def forward_chop(
     shave *= scale
 
     output = input_image.new(b, c, h, w)
+    
     merging_time = ut.timer()
     output[:, :, 0:h_half, 0:w_half] = sr_list[0][:, :, 0:h_half, 0:w_half]
     output[:, :, 0:h_half, w_half:w] = sr_list[1][
@@ -84,6 +87,7 @@ def forward_chop(
     ]
     merging_time = merging_time.toc()
     timer[2] += merging_time
+    
     return output
 
 
@@ -91,7 +95,8 @@ def forward_chop(
 @click.option("--img_path", default="data/slices/12.npz", help="Path of the image")
 @click.option("--model_name", default="RRDB", help="Name of the model")
 @click.option("--patch_dimension", default=293, help="Patch dimension")
-def main(img_path, model_name, patch_dimension):
+@click.option("--save_mode", default=None, help="Save output as: npy, npz or img")
+def main(img_path, model_name, patch_dimension, save_mode):
     """
     Driver for recursive forward chop. Takes an image path and model name to upsample the image.
     """
@@ -100,7 +105,7 @@ def main(img_path, model_name, patch_dimension):
     img = None
     model = None
     print("\nLoading model and image... \n")
-    time = ut.timer()
+    time_with_model = ut.timer()
     if model_name in ["EDSR"]:
         img = ut.load_image(img_path)
         img = img.unsqueeze(0)
@@ -111,16 +116,18 @@ def main(img_path, model_name, patch_dimension):
         model = md.load_rrdb(device="cuda")
 
     # Timers for saving stats
-    timer = [0, 0, 0, 0, 0]
+    timer = [0, 0, 0, 0, 0, 0]
 
     print("Processing...")
-
-    # Shfiting input image to CUDA
     total_time = ut.timer()
+    
+    # Shfiting input image to CUDA
+
     cpu2gpu_time = ut.timer()
     img = img.to("cuda")
     cpu2gpu_time = cpu2gpu_time.toc()
-
+    timer[0] = cpu2gpu_time
+    
     # Forward chopping and upsampling
     output = forward_chop(
         img, model, timer=timer, min_size=patch_dimension * patch_dimension
@@ -130,18 +137,40 @@ def main(img_path, model_name, patch_dimension):
     gpu2cpu_time = ut.timer()
     output = output.to("cpu")
     gpu2cpu_time = gpu2cpu_time.toc()
+    
     if model_name in ["EDSR"]:
         output = output.int()
+        
     total_time = total_time.toc()
-    time = time.toc()
-    print('Total time: ', time)
-    timer[0] = cpu2gpu_time
-    timer[-2] = gpu2cpu_time
-    timer[-1] = total_time
+    
+    time_with_model = time_with_model.toc()
+
+    timer[-3] = gpu2cpu_time
+    timer[-2] = total_time
+    timer[-1] = time_with_model
     
     # Saving output
-    np.savez("results/recursive_outputx4.npz", output)
-    np.save("results/recursive_outputx4", output)
+    file_name = img_path.split("/")[-1].split(".")[0] + "_recursive_" + str(patch_dimension) + "_output_x4"
+    if save_mode == "npz":
+        np.savez("output_images/" + file_name + ".npz", output)
+    elif save_mode == "npy":
+        np.save("output_images/" + file_name, output)
+    elif save_mode == "img":
+        output = torch.tensor(output).int()
+        output_folder = "output_images"
+        if len(output.shape) == 3:
+            c, h, w = output.shape
+        elif len(output.shape) == 2:
+            h, w = output.shape
+            output = output.unsqueeze(0)
+        else:
+            b, c, h, w = output.shape
+            output = output[0]
+        ut.save_image(
+            output, output_folder, h, w, 1, output_file_name=file_name, add_date=False
+        )
+    else:
+        print('Not saving output')
 
     # Printing processing times
     print("\nCPU 2 GPU time: ", timer[0])
@@ -149,6 +178,7 @@ def main(img_path, model_name, patch_dimension):
     print("\nMerging time: ", timer[2])
     print("\nGPU 2 CPU time", timer[3])
     print("\nTotal execution time: ", timer[4])
+    print("\nTotal time with model loading: ", timer[5])
 
 
 def helper_rrdb_experiment(img_dimension, patch_dimension):
